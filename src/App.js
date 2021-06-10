e4a1184508c902a38a0155756a80c2feadbd05eb
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-10 18:51:52 +0530

    Added Colorhelper using chroma.js to generate different shades

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Palette from './Components/Palette/Palette';
 import seedPalettes from './seedPalettes';
+import { generatePalettes } from './ColorHelper';
 
 function App() {
+  console.log(generatePalettes(seedPalettes[7]));
   return (
     <div>
       <Palette {...seedPalettes[7]} />
