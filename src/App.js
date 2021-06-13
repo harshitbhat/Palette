bf96e02d3cd7f61b58aeec52fc3b0df20f755925
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-13 10:52:28 +0530

    Added Palette Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+
 import Palette from './Components/Palette/Palette';
 import seedPalettes from './seedPalettes';
 import { generatePalettes } from './ColorHelper';
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalettes(seedPalettes[7])} />
-    </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedPalettes.find((palette) => palette.id === id);
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette List goes here.</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalettes(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default App;
+
+/*
+
+<div>
+  <Palette palette={generatePalettes(seedPalettes[7])} />
+</div> 
+
+*/
