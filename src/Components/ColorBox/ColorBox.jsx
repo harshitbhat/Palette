8b3fbf568fcec9f9f9877ef8d37761a921d18a58
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-15 19:30:37 +0530

    Finished Refactoring of Colorbox

--- src/Components/ColorBox/ColorBox.jsx
+++ src/Components/ColorBox/ColorBox.jsx
@@ -42,6 +42,7 @@ const styles = {
     textAlign: 'center',
     lineHeight: '30px',
     textTransform: 'uppercase',
+    fontSize: '12px',
   },
   copyButton: {
     color: (props) =>
@@ -62,6 +63,67 @@ const styles = {
     border: 'none',
     textDecoration: 'none',
     opacity: '0',
+  },
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    padding: '10px',
+    left: '0',
+    bottom: '0',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+  },
+  copyOverlay: {
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)',
+  },
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+    position: 'absolute',
+  },
+  copyMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    fontSize: '3rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: '#fff',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px #000',
+      background: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase',
+    },
+    '& p': {
+      fontSize: '2rem',
+      fontWeight: '100',
+    },
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.3s',
   },
 };
 
@@ -86,14 +148,20 @@ class ColorBox extends Component {
         <div style={{ background: background }} className={classes.ColorBox}>
           <div
             style={{ background: background }}
-            className={`copy-overlay ${copied && 'show'}`}
+            className={`${classes.copyOverlay} ${
+              copied && classes.showOverlay
+            }`}
           ></div>
-          <div className={`copy-message ${copied && 'show'}`}>
+          <div
+            className={`${classes.copyMessage} ${
+              copied && classes.showMessage
+            }`}
+          >
             <h1>Copied!</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.copyButton}>Copy</button>
@@ -103,7 +171,7 @@ class ColorBox extends Component {
               to={`/palette/${paletteId}/${id}`}
               onClick={(e) => e.stopPropagation()}
             >
-              <span className={classes.seeMore}>MORE</span>
+              <span className={classes.seeMore}>more</span>
             </Link>
           )}
         </div>
